2392a942ddbe373647a030322d246e5e7675ed39
Simon Albicocco <100119048+simonalbcc@users.noreply.github.com>
2025-07-27 18:08:03 +0200

    fix build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-  content: ["./src/**/*.{html,ts,css,scss}"],
+  content: ["./src/**/*.{html,ts}"],
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
     require("daisyui")
-  ]
+  ],
+  daisyui: {
+    themes: [{
+      custom: {
+        "primary": "#152a65",
+        "primary-hover": "#1e40af",
+        // ... autres couleurs
+      }
+    }]
+  }
 }
